d9106db43694b4627f54ef11bc40ec0bed45e84b
Ishan Gupta <ishangupta97@gmail.com>
2016-10-27 12:48:40 +0530

    meta data added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>My Tokri | Question Bank</title>
 	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> -->
